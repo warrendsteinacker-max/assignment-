30211228cbaf077a880c84b2782c2d8bd88db315
Warren <warrendsteinacker@gmail.com>
2026-01-25 16:32:47 -0500

    makeing other headings

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,6 +9,10 @@ function App() {
     <>
     <img src='Gemini_Generated_Image_1.png' style={{borderRadius: '10px', width: '500px', height: '500px'}}/>
     <h6 style={{fontSize: '5rem'}}>Oral Language: The Foundation for Early Spelling and Writing</h6>
+
+    <h6 style={{fontSize: '3rem', borderBottom: '10px solid black'}}>Introduction: Why Talking Builds Writers</h6>
+
+    <h6 style={{fontSize: '3rem', borderBottom: '10px solid black'}}>Home Language as a Strength</h6>
     
     
     </>
