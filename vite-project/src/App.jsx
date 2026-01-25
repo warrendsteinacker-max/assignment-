7033f7fa176155c30496fc6221dd95dbcfa77e06
Warren <warrendsteinacker@gmail.com>
2026-01-25 16:03:35 -0500

    made proper cahnges

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 // import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 //will use this fir creating actual website
 import './App.css'
