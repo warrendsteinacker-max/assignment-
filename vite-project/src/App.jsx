c952b43a8fecd6d841023e3577078a4efe5c2ad9
Warren <warrendsteinacker@gmail.com>
2026-01-25 16:28:16 -0500

    adding headings

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,6 +8,9 @@ function App() {
   return (
     <>
     <img src='Gemini_Generated_Image_1.png' style={{borderRadius: '10px', width: '500px', height: '500px'}}/>
+    <h6 style={{fontSize: '5rem'}}>Oral Language: The Foundation for Early Spelling and Writing</h6>
+    
+    
     </>
   )
 }
