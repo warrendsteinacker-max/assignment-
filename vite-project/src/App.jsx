58945f5a76df904bae1ba43184fc5aef449f8535
Warren <warrendsteinacker@gmail.com>
2026-01-25 16:24:58 -0500

    adding stuff

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,8 +8,6 @@ function App() {
   return (
     <>
     <img src='Gemini_Generated_Image_1.png' style={{borderRadius: '10px', width: '500px', height: '500px'}}/>
-    <p>hello</p>
-    <p>1</p>
     </>
   )
 }
