d043b94b7f612e544a405bec126903dacee6f90f
Warren <warrendsteinacker@gmail.com>
2026-01-25 15:09:17 -0500

    fix

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,8 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-bundle'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Or '/your-repo-name/'
+  // Use './' if you aren't sure of the repo name, 
+  // but it's better to use '/YOUR_REPO_NAME/' for GitHub Pages
+  base: './', 
   plugins: [react()],
 })
