eeef070780b05156dd220ced640640eb0226912a
Warren <warrendsteinacker@gmail.com>
2026-01-25 12:42:41 -0500

    n

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-bundle'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Or '/your-repo-name/'
   plugins: [react()],
-  base: '/assignment-/', // This must match your repo name in GitHub
 })
