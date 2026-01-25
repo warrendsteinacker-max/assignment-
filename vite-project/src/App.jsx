e076e79e35caacf893916c45e2f71b262df85123
Warren <warrendsteinacker@gmail.com>
2026-01-25 16:14:42 -0500

    added better background

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-    <img src='Gemini_Generated_Image_1.png' style={{width: '100px', height: '100px'}}/>
+    <img src='Gemini_Generated_Image_1.png' style={{borderRadius: '10px', width: '500px', height: '500px'}}/>
     <p>hello</p>
     <p>1</p>
     </>
