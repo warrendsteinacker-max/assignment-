fef0464a8d1707d6f5563cbd68f7188be21e1e03
Warren <warrendsteinacker@gmail.com>
2026-01-29 11:12:43 -0500

    doing practice

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,10 @@
 // import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 //will use this fir creating actual website
 import './App.css'
+import Mod1 from './comps/Mod1'
+import Mod2 from './comps/Mod2'
+import Mod3 from './comps/Mod3'
+import Home from './comps/Home'
 
 function App() {
 
