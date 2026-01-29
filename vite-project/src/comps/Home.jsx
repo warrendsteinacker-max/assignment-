2d38d246595f205397842c65a14da60b6697cc5b
Warren <warrendsteinacker@gmail.com>
2026-01-29 13:43:17 -0500

    doing some prac

--- vite-project/src/comps/Home.jsx
+++ vite-project/src/comps/Home.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <h1 style={{color: 'black', fontWeight: 'bold', fontSize: '5rem'}}> My Elementary Literacy Skills Website</h1>
+        <div>
+            <h1 style={{color: 'black', fontSize: '2rem'}}>Welcome to our Literacy Skills Website!</h1>
+            <p>This website is a comprehensive overview that documents the literacy training process covered in this class. It tracks the progression from the synthesis of assignments across eight distinct ones, part 1 through part 8.</p>
+        </div>
+    </div>
   )
 }
 
