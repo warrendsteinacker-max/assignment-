8ebc7b61b7f6aad0bb8a24972b980ac68034d98b
Warren <warrendsteinacker@gmail.com>
2026-02-17 11:25:07 -0500

    made mod 6 page

--- vite-project/src/comps/Mod5.jsx
+++ vite-project/src/comps/Mod5.jsx
@@ -4,6 +4,13 @@ const Mod5 = () => {
 
     const pdfPath1 = `./Teacher Prep Lesson Plan Format (5) (1).pdf`; 
     const pdfPath2 = `./part a mod 5 and one page reflection (1).pdf`;
+    const pdfPath3 = `./1-Zac-by-Starfall (1).pdf`;
+    const pdfPath4 = `./7-Pete-by-Starfall (1).pdf`;
+    const pdfPath5 = `./10-DuneBuggy-by-Starfall (1).pdf`;
+    const pdfPath6 = `./11-SoapBoat-by-Starfall (1).pdf`;
+    const pdfPath7 = `./12-CarRace-by-Starfall (1).pdf`;
+    const pdfPath8 = `./13-Glory-by-Starfall (1).pdf`;
+    const pdfPath9 = `./14-SurferGirl-by-Starfall (1).pdf`;
     
 
     
@@ -50,7 +57,89 @@ const Mod5 = () => {
         </a>
       </div>
 
+    <iframe 
+        style={{ borderRadius: '15px', width: '100%', height: '600px', border: '2px solid #eaeaea', marginBottom: '10px' }}
+        src={pdfPath3} title="PDF 2"
+      ></iframe>
+      <div style={{ textAlign: 'center', marginBottom: '50px' }}>
+        <a href={pdfPath3} download>
+          <button style={{ backgroundColor: '#5A90E2', color: 'white', padding: '12px 25px', borderRadius: '8px', border: 'none', cursor: 'pointer', fontWeight: 'bold' }}>
+            Download Book For Assigment 
+          </button>
+        </a>
+      </div>
 
+    <iframe 
+        style={{ borderRadius: '15px', width: '100%', height: '600px', border: '2px solid #eaeaea', marginBottom: '10px' }}
+        src={pdfPath4} title="PDF 2"
+      ></iframe>
+      <div style={{ textAlign: 'center', marginBottom: '50px' }}>
+        <a href={pdfPath4} download>
+          <button style={{ backgroundColor: '#5A90E2', color: 'white', padding: '12px 25px', borderRadius: '8px', border: 'none', cursor: 'pointer', fontWeight: 'bold' }}>
+            Download Book For Assigment 
+          </button>
+        </a>
+      </div>
+
+    <iframe 
+        style={{ borderRadius: '15px', width: '100%', height: '600px', border: '2px solid #eaeaea', marginBottom: '10px' }}
+        src={pdfPath5} title="PDF 2"
+      ></iframe>
+      <div style={{ textAlign: 'center', marginBottom: '50px' }}>
+        <a href={pdfPath5} download>
+          <button style={{ backgroundColor: '#5A90E2', color: 'white', padding: '12px 25px', borderRadius: '8px', border: 'none', cursor: 'pointer', fontWeight: 'bold' }}>
+            Download Book For Assigment 
+          </button>
+        </a>
+      </div>
+
+    <iframe 
+        style={{ borderRadius: '15px', width: '100%', height: '600px', border: '2px solid #eaeaea', marginBottom: '10px' }}
+        src={pdfPath6} title="PDF 2"
+      ></iframe>
+      <div style={{ textAlign: 'center', marginBottom: '50px' }}>
+        <a href={pdfPath6} download>
+          <button style={{ backgroundColor: '#5A90E2', color: 'white', padding: '12px 25px', borderRadius: '8px', border: 'none', cursor: 'pointer', fontWeight: 'bold' }}>
+            Download Book For Assigment 
+          </button>
+        </a>
+      </div>
+
+    <iframe 
+        style={{ borderRadius: '15px', width: '100%', height: '600px', border: '2px solid #eaeaea', marginBottom: '10px' }}
+        src={pdfPath7} title="PDF 2"
+      ></iframe>
+      <div style={{ textAlign: 'center', marginBottom: '50px' }}>
+        <a href={pdfPath7} download>
+          <button style={{ backgroundColor: '#5A90E2', color: 'white', padding: '12px 25px', borderRadius: '8px', border: 'none', cursor: 'pointer', fontWeight: 'bold' }}>
+            Download Book For Assigment 
+          </button>
+        </a>
+      </div>
+
+    <iframe 
+        style={{ borderRadius: '15px', width: '100%', height: '600px', border: '2px solid #eaeaea', marginBottom: '10px' }}
+        src={pdfPath8} title="PDF 2"
+      ></iframe>
+      <div style={{ textAlign: 'center', marginBottom: '50px' }}>
+        <a href={pdfPath8} download>
+          <button style={{ backgroundColor: '#5A90E2', color: 'white', padding: '12px 25px', borderRadius: '8px', border: 'none', cursor: 'pointer', fontWeight: 'bold' }}>
+            Download Book For Assigment 
+          </button>
+        </a>
+      </div>
+
+    <iframe 
+        style={{ borderRadius: '15px', width: '100%', height: '600px', border: '2px solid #eaeaea', marginBottom: '10px' }}
+        src={pdfPath9} title="PDF 2"
+      ></iframe>
+      <div style={{ textAlign: 'center', marginBottom: '50px' }}>
+        <a href={pdfPath9} download>
+          <button style={{ backgroundColor: '#5A90E2', color: 'white', padding: '12px 25px', borderRadius: '8px', border: 'none', cursor: 'pointer', fontWeight: 'bold' }}>
+            Download Book For Assigment 
+          </button>
+        </a>
+      </div>
 
     </div>
   )
